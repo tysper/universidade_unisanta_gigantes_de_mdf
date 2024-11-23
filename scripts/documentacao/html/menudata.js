64210a7fca2cb08084f29729afd1e5653f5fbca6
Tysper <pedropaulo_nunes@outlook.com>
2024-11-23 04:30:07 -0300

    Correções de traduções

--- scripts/documentacao/html/menudata.js
+++ scripts/documentacao/html/menudata.js
@@ -23,11 +23,11 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"},
-{text:"Globals",url:"globals.html",children:[
-{text:"All",url:"globals.html",children:[
+{text:"Página Inicial",url:"index.html"},
+{text:"Arquivos",url:"files.html",children:[
+{text:"Lista de Arquivos",url:"files.html"},
+{text:"Globais",url:"globals.html",children:[
+{text:"Todos",url:"globals.html",children:[
 {text:"a",url:"globals.html#index_a"},
 {text:"b",url:"globals.html#index_b"},
 {text:"c",url:"globals.html#index_c"},
@@ -43,6 +43,6 @@ var menudata={children:[
 {text:"s",url:"globals.html#index_s"},
 {text:"t",url:"globals.html#index_t"},
 {text:"u",url:"globals.html#index_u"}]},
-{text:"Functions",url:"globals_func.html"},
-{text:"Variables",url:"globals_vars.html"},
+{text:"Funções",url:"globals_func.html"},
+{text:"Variáveis",url:"globals_vars.html"},
 {text:"Macros",url:"globals_defs.html"}]}]}]}
